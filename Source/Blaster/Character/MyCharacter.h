6781ecb1622d9adec29ba2923a887af572ef1b29
Bryan Escobar <bryanesc26@gmail.com>
2023-07-21 14:39:36 -0600

    First commit

---
+++ Source/Blaster/Character/MyCharacter.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MyCharacter.generated.h"
+
+UCLASS()
+class BLASTER_API AMyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	AMyCharacter();
+
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class USpringArmComponent* CameraBoom;
+	
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class UCameraComponent* FollowCamera;
+	
+
+};
